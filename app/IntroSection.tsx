8b01b3c779cc8806b67076c128756da494181a70
Manu G <manusvathgurudath@gmail.com>
2023-12-03 03:59:39 +0000

    W

--- app/IntroSection.tsx
+++ app/IntroSection.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { orange } from "@radix-ui/colors";
 
 export function IntroSection() {
   return (
@@ -8,22 +7,22 @@ export function IntroSection() {
         <span className="material-symbols-rounded">location_on</span>
         California, USA
       </div>
-      <h1 className="text-5xl mb-2">
+      <h1 className="text-5xl mb-2 title max-w-md text-orange-950">
         I&apos;m Manu, a full stack software enthusiast.{" "}
       </h1>
-      <p className="max-w-md">
+      <p className="max-w-md opacity-90">
         With a passion for building things, I&apos;m always looking for new
         opportunities to learn and grow.
       </p>
       <div className="gap-2 flex mt-4">
         <button
-          className="bg-purple-500 btn"
+          className="bg-purple-500 btn text-purple-950"
           onClick={() => window.open("https://linkedin.com/in/manu-codes")}
         >
           LinkedIn
         </button>
         <button
-          className="bg-orange-500 btn"
+          className="bg-orange-500 btn text-orange-950"
           onClick={() => window.open("//github.com/manuthecoder")}
         >
           GitHub
--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,7 @@
 "use client";
-import Image from "next/image";
-import { StyleHTMLAttributes } from "react";
 import { IntroSection } from "./IntroSection";
 import { Skills } from "./Skills";
-
-function Projects() {
-  return (
-    <div>
-      <h2 className="subheading">Projects</h2>
-      <div className="flex flex-col gap-3">
-        {[
-          {
-            name: "Dysperse",
-            status: "New",
-            description: "Meet the new standard for productivity.",
-            href: "//dysperse.com",
-          },
-          {
-            name: "Popvote",
-            status: false,
-            description: "Meet the new standard for productivity.",
-            href: "//popvote.vercel.app",
-          },
-          {
-            name: "SchoolNerd",
-            status: "Deprecated",
-            description:
-              "Google classroom, with superpowers. Winner of HackOR 2021",
-            href: "https://devpost.com/software/schoolnerd",
-          },
-        ].map((project) => (
-          <div
-            className="card"
-            key={project.name}
-            onClick={() => {
-              if (project.href) window.open(project.href);
-            }}
-          >
-            <Image
-              src={`/projects/${project.name.toLowerCase()}.png`}
-              width={50}
-              height={50}
-              alt={`${project.name} logo`}
-              className="card-logo"
-            />
-            <div>
-              <h3 className="card-title font-bold flex items-center gap-2">
-                {project.name}
-                {project.status && (
-                  <div className="chip bg-orange-200 font-medium text-xs">
-                    {project.status}
-                  </div>
-                )}
-              </h3>
-              <p className="card-subtitle">{project.description}</p>
-            </div>
-            <span className="material-symbols-rounded arrow">north_east</span>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
+import { Projects } from "./Projects";
 
 export default function Home() {
   return (
