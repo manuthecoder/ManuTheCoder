ba1c6b935f400a8b7068bd60323277ebd260c8db
Manu G <manusvathgurudath@gmail.com>
2024-11-24 22:28:43 -0800

    sadf

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -2,28 +2,31 @@
 
 export function Navbar() {
   return (
-    <div className="flex h-20 items-center px-6 sm:px-10 gap-7">
-      <div className="grow font-extrabold text-xl">Manu G</div>
-      {/* <a href="https://github.com/manuthecoder" className="nav-btn">
+    <>
+      <div className="fixed top-0 left-0 w-full flex h-20 items-center px-6 sm:px-10 gap-7">
+        <div className="grow font-extrabold text-xl">Manu G</div>
+        {/* <a href="https://github.com/manuthecoder" className="nav-btn">
         Github
       </a> */}
-      <div
-        className="nav-btn"
-        onClick={() => {
-          try {
-            navigator.share({
-              text: "Check out Manu G - full stack software enthusiast",
-              url: "https://manu.is-a.dev",
-            });
-          } catch (e) {
-            navigator.clipboard.writeText("https://manu.is-a.dev");
-            alert("Copied link to clipboard!");
-          }
-        }}
-      >
-        <span className="material-symbols-rounded">share</span>
+        <div
+          className="nav-btn"
+          onClick={() => {
+            try {
+              navigator.share({
+                text: "Check out Manu G - full stack software enthusiast",
+                url: "https://manu.is-a.dev",
+              });
+            } catch (e) {
+              navigator.clipboard.writeText("https://manu.is-a.dev");
+              alert("Copied link to clipboard!");
+            }
+          }}
+        >
+          <span className="material-symbols-rounded">share</span>
+        </div>
       </div>
-    </div>
+      <div className="h-20" />
+    </>
   );
 }
 
