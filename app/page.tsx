37abd0281f0e131c7dbd9f8eaa1b760f81ef1a06
Manu G <manusvathgurudath@gmail.com>
2024-11-25 19:20:30 -0800

    format

--- app/page.tsx
+++ app/page.tsx
@@ -114,9 +114,7 @@ export default function Home() {
   return (
     <main
       className="p-7 max-w-5xl sm:p-5 sm:px-12 border-2 border-[#431407] rounded-3xl bg-orange-50 mx-auto w-full h-full min-h-screen flex flex-col items-center page-container"
-      style={{
-        boxShadow: `2px 2px 0 4px #431407`,
-      }}
+      style={{ boxShadow: `2px 2px 0 4px #431407` }}
     >
       <div className="mt-20 flex items-start sm:items-center flex-col max-w-3xl sm:text-center">
         <IntroSection />
