90263b68ae6536cb8f46344f66ac1bfb3c7b4da7
Manu G <manusvathgurudath@gmail.com>
2025-01-15 21:06:11 -0800

    add redirect configuration for manu.is-a.dev to bymanu.me

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: "/:slug*",
+                has: [
+                    {
+                        type: "host",
+                        value: "manu.is-a.dev",
+                    },
+                ],
+                destination: "https://bymanu.me/:slug*",
+                permanent: true,
+            },
+        ];
+    },
+}
 
 module.exports = nextConfig
