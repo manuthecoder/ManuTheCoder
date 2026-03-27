bbd3c49c285ce6f435310c2843210804a3f3958d
Manu Gurudath <manu@Manus-MacBook-Pro.local>
2026-03-27 09:40:29 -0700

    asdf

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,6 @@ function Spotify() {
           <div
             onMouseOver={() => mutate()}
             className="chip bg-red-900 text-red-50 font-medium text-xs"
-            title="yes, this s what I am actually listening to!"
           >
             <div className="animate-pulse w-1.5 h-1.5 rounded-full bg-red-50"></div>
             LIVE
